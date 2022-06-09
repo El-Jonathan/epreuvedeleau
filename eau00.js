5144b7d3457f87d2715e9614d767f844de53e6ba
El-Jonathan <jonathan.elana@hotmail.fr>
2022-06-09 12:37:26 +0200

    avancé

---
+++ eau00.js
@@ -0,0 +1,15 @@
+// Combinaison de 3 chiffres
+
+
+for (let i =0; i<=7;i ++){
+    
+    for (let j = i + 1; j<=8; j++){
+
+        for (let k = j+1; k<=9;k++){
+
+            console.log(i,j,k);
+        }
+    }
+
+}
+
