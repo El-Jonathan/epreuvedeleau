64437a9a72995c601b8723bfcba7b34612ecd617
El-Jonathan <jonathan.elana@hotmail.fr>
2022-06-23 10:34:56 +0200

    modif

--- eau02.js
+++ eau02.js
@@ -2,8 +2,15 @@
 
 let phr = `"codeur" "je" "suis"`;
 
+function reverse (){
+
 let envers = phr.split(" ").reverse();
     console.log(envers);
 
-envers.forEach(mots => console.log(mots));
-
+envers.forEach(mots => console.log(mots)){
+    if (mots == undefined){
+        (console.log("error"))
+        return;
+    };
+}
+}
